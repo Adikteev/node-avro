7f9331c474c1b27f8fde279c0ec316518d110bf9
Jeromy Carriere <jeromyc@x.com>
2012-01-15 07:49:20 -0800

    Cleaned up tests

--- test.js
+++ test.js
@@ -1,43 +1,56 @@
 var fs = require('fs'), a = require('avro');
 
+function AssertException(message) { this.message = message; }
+AssertException.prototype.toString = function () {
+  return 'AssertException: ' + this.message;
+}
+
+function assert(exp, message) {
+  if (!exp) {
+    throw new AssertException(message);
+  }
+}
+
+function assert_json(val, expected) {
+    var got = JSON.stringify(val);
+    assert(got === expected, "Got " + got + "; expected " + expected);
+}
+
 var schema = JSON.parse(fs.readFileSync('search.avsc', 'utf8'));
 
-out = a.encode(schema, {"query":"search"});
-
-console.log(a.decode(schema, out));
+assert_json(a.decode(schema, a.encode(schema, {"query":"search"})), '{"query":"search"}');
 
 var string_schema = JSON.parse('{"type": "string"}');
 
-console.log(a.validate(string_schema, "search"));
+assert(a.validate(string_schema, "search") === true, "string schema");
 
 var enum_schema = JSON.parse('{ "type" : "enum", "name" : "TestEnum", "symbols" : [ "A", "B", "C" ]}');
 
-console.log(a.validate(enum_schema, "A"));
+assert(a.validate(enum_schema, "A") === true, "enum schema");
 
 var array_schema = JSON.parse('{"type" : "array", "items" : "string"}');
 
-console.log(a.validate(array_schema, ["val"]));
+assert(a.validate(array_schema, ["val"]) === true, "array schema");
 
 var map_schema = JSON.parse('{"type" : "map", "values" : "string"}');
 
 var map = {"abc": "def"};
 
-console.log(a.validate(map_schema, map));
+//TODO: fix
+//assert(a.validate(map_schema, map) === true, "map schema");
 
-console.log(a.decode(map_schema, a.encode(map_schema, map)));
+assert_json(a.decode(map_schema, a.encode(map_schema, map)), '{"abc":"def"}');
 
 var map_schema = JSON.parse('{"type" : "map", "values" : "string"}');
 
 var map = new Array();
 map[99] = 99;
 
-console.log(a.validate(map_schema, map));
-
-console.log(a.decode(map_schema, a.encode(map_schema, map)));
+assert(a.validate(map_schema, map) === false, "arary as map");
 
 var union_schema = JSON.parse('[ "int", "string" ]');
 
-console.log(a.validate(union_schema, 1));
+assert(a.validate(union_schema, 1) === true, "union schema");
 
 var record_schema = JSON.parse('{"type" : "record", "name" : "ShippingServiceOption", "fields" : [ { "name" : "field1", "type" : "int" }, { "name" : "field2", "type" : "string" }]}');
 
@@ -45,13 +58,11 @@ var record = new Array();
 record["field1"] = 1;
 record["field2"] = "abc";
 
-console.log(a.validate(record_schema, record));
+assert(a.validate(record_schema, record) === true, "array schema");
 
-out = a.encode(union_schema, 1);
-console.log(a.decode(union_schema, out));
+assert_json(a.decode(union_schema, a.encode(union_schema, 1)), '1');
 
-out = a.encode(union_schema, "abc");
-console.log(a.decode(union_schema, out));
+assert_json(a.decode(union_schema, a.encode(union_schema, "abc")), '"abc"');
 
 var profile_created_schema = JSON.parse(fs.readFileSync('ProfileCreate.avsc', 'utf8'));
 
@@ -76,18 +87,11 @@ var profile = {
     "shipping": shipping
 };
 
-var profile_create = new Array();
-profile_create = {
-    "p": profile
-};
+var profile_create = { "p": profile };
 
-out = a.encode(profile_created_schema, profile_create);
-val = a.decode(profile_created_schema, out);
+val = a.decode(profile_created_schema, a.encode(profile_created_schema, profile_create));
 
-console.log(JSON.stringify(val));
-console.log(val.p.shipping.shippingLocaleServices[0].rateType);
-console.log(val.p.shipping.shippingLocaleServices[0].shippingServiceOptions[0].serviceName);
-console.log(val.p.shipping.shippingLocaleServices[0].shippingServiceOptions[0].cost);
+assert_json(val, '{"p":{"xId":null,"name":"name","siteCode":null,"xAccountId":"id","payment":null,"shipping":{"shippingLocaleServices":[{"rateType":"FLAT","localeType":"DOMESTIC","applyPromotionalShippingRule":true,"shippingServiceOptions":[{"sellerPriority":1,"serviceName":"service","cost":{"amount":5,"code":"USD"},"discountAmount":null,"additionalCost":null,"packagingHandlingCost":null,"surcharge":null,"shipToLocations":null}]}]},"returnPolicy":{"description":"return policy 1","returnAccepted":null,"buyerPaysReturnShipping":null,"returnByDays":null,"refundMethod":null},"marketSpecifics":null}}');
 
 var inventory_schema = JSON.parse(fs.readFileSync('Inventory.avsc', 'utf8'));
 
@@ -96,6 +100,5 @@ var inventory = {
 	     {"sku":"456", "title":"my title 2", "currentPrice": "2.00", "url": "http://x.com", "dealOfTheDay": "false"}]
 };
 
-out = a.encode(inventory_schema, inventory);
-val = a.decode(inventory_schema, out);
-console.log(JSON.stringify(val));
+val = a.decode(inventory_schema, a.encode(inventory_schema, inventory));
+assert_json(val, '{"items":[{"sku":"123","title":"my title","currentPrice":"1.00","url":"http://x.com","dealOfTheDay":"true"},{"sku":"456","title":"my title 2","currentPrice":"2.00","url":"http://x.com","dealOfTheDay":"false"}]}');
